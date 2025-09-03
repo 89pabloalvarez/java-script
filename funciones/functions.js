5fb5d7a9bfc10ea329a43c71cb51746a673b5f60
Paul Rammone <89.pablo.alvarez@gmail.com>
2025-09-03 14:48:15 -0300

    DRY - agrego función de validación de que el campo ingresado es string (aunque todo prompt es string) y no este vacío. utilizo en cada campo del formulario.

--- funciones/functions.js
+++ funciones/functions.js
@@ -5,13 +5,13 @@ export function inputUsuario(nuevoUsuario) {
     let usuario = "";
     do {
         usuario = prompt(`Datos del usuario: \n\nUsuario: ${nuevoUsuario.usuario} \nEmail: ${nuevoUsuario.email} \nRol: ${nuevoUsuario.rol} \n\nIngrese el nombre de usuario:`)?.trim();
-    if (usuario === "") {
+    if (!validarInputNoVacio(usuario)) {
         alert(`El nombre de usuario no puede estar vacío. Intente nuevamente.`)
     } else if (validarUsuarioExistente(usuario)) {
         alert(`El usuario "${usuario}" ya existe. Por favor, ingrese un nombre de usuario diferente.`)
         usuario = prompt(`Datos del usuario: \n\nUsuario: ${nuevoUsuario.usuario} \nEmail: ${nuevoUsuario.email} \nRol: ${nuevoUsuario.rol} \n\nIngrese el nombre de usuario:`)
     }
-  } while (usuario === "" || validarUsuarioExistente(usuario));
+  } while (!validarInputNoVacio(usuario) || validarUsuarioExistente(usuario));
 
   return usuario;
 }
@@ -20,12 +20,14 @@ export function inputEmail(nuevoUsuario) {
     let email = ""
     do {
         email = prompt(`Datos del usuario: \n\nUsuario: ${nuevoUsuario.usuario} \nEmail: ${nuevoUsuario.email} \nRol: ${nuevoUsuario.rol} \n\nEl formato correcto es: "correo@electronico.com"\n\nIngrese el correo electrónico:`)?.trim()
-        if (!validarFormatoEmail(email)) {
+        if (!validarInputNoVacio(email)) {
+            alert(`El Email ingresado no puede estar vacío. Intente nuevamente.`)
+        } else if (!validarFormatoEmail(email)) {
             alert(`El Email ingresado: "${email}" es inválido.\nIntente con un formato como éste ejemplo: "correo@electronico.com"`)
         } else if (validarEmailExistente(email)) {
             alert(`El Email "${email}" ya existe. Por favor, ingrese un correo electrónico diferente.`)
         }
-    } while (!validarFormatoEmail(email))
+    } while (!validarInputNoVacio(email) || !validarFormatoEmail(email) || validarEmailExistente(email))
     return email
 }
 
@@ -33,10 +35,12 @@ export function inputRol(nuevoUsuario) {
     let rol = ""
     do {
         rol = prompt(`Datos del usuario: \n\nUsuario: ${nuevoUsuario.usuario} \nEmail: ${nuevoUsuario.email} \nRol: ${nuevoUsuario.rol} \n\nLos roles permitidos son:\n"${rolesValidos[0]}", "${rolesValidos[1]}" y "${rolesValidos[2]}"\n\n ¿Qué rol tendría el nuevo usuario?:`).toLowerCase()
-        if (!validarRol(rol)) {
+        if (!validarInputNoVacio(rol)) {
+            alert(`El rol ingresado esta vacío. ingrese alguno de los siguientes roles permitidos: \n\n"${rolesValidos[0]}", "${rolesValidos[1]}" o "${rolesValidos[2]}"`)
+        } else if (!validarRol(rol)) {
             alert(`El rol ingresado: "${rol}" es inválido. \n\nLos roles permitidos son:\n"${rolesValidos[0]}", "${rolesValidos[1]}" y "${rolesValidos[2]}"\n\n Ingrese un rol válido.`)
         }
-    } while (!validarRol(rol))
+    } while (!validarInputNoVacio(rol) || !validarRol(rol))
   return rol
 }
 
@@ -46,12 +50,12 @@ export function inputContraseña(nuevoUsuario) {
     do {
         contraseña = prompt(`Datos del usuario: \n\nUsuario: ${nuevoUsuario.usuario} \nEmail: ${nuevoUsuario.email} \nRol: ${nuevoUsuario.rol} \n\nAhora ingrese la contraseña:`).trim()
         confirmacionContraseña = prompt(`Datos del usuario: \n\nUsuario: ${nuevoUsuario.usuario} \nEmail: ${nuevoUsuario.email} \nRol: ${nuevoUsuario.rol} \n\nVuelva a ingresar la contraseña:`).trim()
-        if (contraseña === "" || confirmacionContraseña === "") {
+        if (!validarInputNoVacio(contraseña) || !validarInputNoVacio(confirmacionContraseña)) {
             alert(`Debe ingresar una contraseña, no puede estar vacío.\n\nIntente nuevamente.`)
         } else if (contraseña !== confirmacionContraseña) {
             alert(`Las contraseñas ingresadas "${contraseña}" y "${confirmacionContraseña}" no coinciden. Intente nuevamente.`)
         }
-    } while (contraseña !== confirmacionContraseña || contraseña === "" || confirmacionContraseña === "")
+    } while (!validarInputNoVacio(contraseña) || !validarInputNoVacio(confirmacionContraseña) || contraseña !== confirmacionContraseña)
     alert("Contraseña confirmada correctamente.")
     return contraseña
 }
@@ -61,7 +65,10 @@ export function nextIdUsuario() {
     return usuarios.length === 0 ? 1 : Math.max(...usuarios.map(user => user.id)) + 1
 }
 
-//Función para validar si el usuario ya existe en la "base de datos".
+function validarInputNoVacio(input) {
+  return typeof input === "string" && input.trim().length > 0;
+}
+
 function validarUsuarioExistente(usuario) {
     return usuarios.some(user => user.usuario.toLowerCase() === usuario.toLowerCase())
 }
@@ -78,12 +85,10 @@ function validarRol(rol) {
     return rolesValidos.includes(rol)
 }
 
-//Función para mostrar la lista actualizada de usuarios en la consola.
+//Funciones para mostrar la lista de usuarios.
 export function mostrarUsuariosEnConsola() {
     console.log('la lista actual de usuarios en la "DB" es:', usuarios)
 }
-
-//Función para mostrar la lista actualizada de usuarios en pantalla.
 export function mostrarUsuariosEnPantalla() {
     let listaUsuarios = "Lista de usuarios:\n\n"
     usuarios.forEach(user => {
