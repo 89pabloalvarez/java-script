5eac547ba6094f717208e9f3569935723ff2a3cc
Paul Rammone <89.pablo.alvarez@gmail.com>
2025-09-03 13:11:24 -0300

    Modularizo las funciones para atomizar main.js

--- funciones/functions.js
+++ funciones/functions.js
@@ -1,18 +1,68 @@
-//Inicializar la Base de Datos simulada con algunos usuarios predefinidos.
-export function startDB() {
-  fetch('./DB/usuarios.json')
-    .then(res => res.json())
-    .then(data => {
-      usuarios = data;
-    });
+import { usuarios } from '../DB/usuarios.js';
+import { rolesValidos } from '../DB/roles.js';
+
+export function inputUsuario(nuevoUsuario) {
+    let usuario = prompt(`Datos del usuario: \n\nUsuario: ${nuevoUsuario.usuario} \nEmail: ${nuevoUsuario.email} \nRol: ${nuevoUsuario.rol} \n\nIngrese el nombre de usuario:`);
+    //Valido que el usuario no exista en la "base de datos".
+    while (validarUsuarioExistente(usuario)) {
+        alert(`El usuario "${usuario}" ya existe. Por favor, ingrese un nombre de usuario diferente.`);
+        usuario = prompt(`Datos del usuario: \n\nUsuario: ${nuevoUsuario.usuario} \nEmail: ${nuevoUsuario.email} \nRol: ${nuevoUsuario.rol} \n\nIngrese el nombre de usuario:`);
+    }
+    return usuario;
+}
+
+export function inputEmail(nuevoUsuario) {
+    let email = "";
+    do {
+        email = prompt(`Datos del usuario: \n\nUsuario: ${nuevoUsuario.usuario} \nEmail: ${nuevoUsuario.email} \nRol: ${nuevoUsuario.rol} \n\nEl formato correcto es: "correo@electronico.com"\n\nIngrese el correo electrónico:`)?.trim();
+        if (!validarFormatoEmail(email)) {
+            alert(`El Email ingresado: "${email}" es inválido.\nIntente con un formato como éste ejemplo: "correo@electronico.com"`);
+        }
+    } while (!validarFormatoEmail(email));
+    return email;
+}
+
+export function inputRol(nuevoUsuario) {
+    let rol = "";
+    do {
+        rol = prompt(`Datos del usuario: \n\nUsuario: ${nuevoUsuario.usuario} \nEmail: ${nuevoUsuario.email} \nRol: ${nuevoUsuario.rol} \n\nLos roles permitidos son:\n"${rolesValidos[0]}", "${rolesValidos[1]}" y "${rolesValidos[2]}"\n\n ¿Qué rol tendría el nuevo usuario?:`).toLowerCase();
+        if (!validarRol(rol)) {
+            alert(`El rol ingresado: "${nuevoUsuario.rol}" es inválido. \n\nLos roles permitidos son:\n"${rolesValidos[0]}", "${rolesValidos[1]}" y "${rolesValidos[2]}"\n\n Ingrese un rol válido.`);
+        }
+    } while (!validarRol(rol));
+  return rol;
+}
+
+export function inputContraseña(nuevoUsuario) {
+    let contraseña = "";
+    let confirmacionContraseña = "";
+    do {
+      contraseña = prompt(`Datos del usuario: \n\nUsuario: ${nuevoUsuario.usuario} \nEmail: ${nuevoUsuario.email} \nRol: ${nuevoUsuario.rol} \n\nAhora ingrese la contraseña:`).trim();
+      confirmacionContraseña = prompt(`Datos del usuario: \n\nUsuario: ${nuevoUsuario.usuario} \nEmail: ${nuevoUsuario.email} \nRol: ${nuevoUsuario.rol} \n\nVuelva a ingresar la contraseña:`).trim();
+      if (contraseña === "" || confirmacionContraseña === "") {
+        alert(`Debe ingresar una contraseña, no puede estar vacío.\n\nIntente nuevamente.`);
+      } else if (contraseña !== confirmacionContraseña) {
+        alert(`Las contraseñas ingresadas "${contraseña}" y "${confirmacionContraseña}" no coinciden. Intente nuevamente.`);
+      }
+    } while (contraseña !== confirmacionContraseña || contraseña === "" || confirmacionContraseña === "");
+    alert("Contraseña confirmada correctamente.");
+    return contraseña;
+}
+
+//Función para validar si el usuario ya existe en la "base de datos".
+function validarUsuarioExistente(nombreUsuario) {
+  return usuarios.some(user => user.usuario.toLowerCase() === nombreUsuario.toLowerCase());
+}
+
+function validarFormatoEmail(email) {
+  return email.includes("@") && email.includes(".");
+}
+
+function validarRol(rol) {
+  return rolesValidos.includes(rol);
 }
 
 //Función para mostrar la lista actualizada de usuarios en la consola.
 export function mostrarUsuariosEnConsola() {
-  console.log('la lista de usuarios en la "DB" es:', usuarios);
-}
-
-//Función para validar si el usuario ya existe en la "base de datos".
-export function validarUsuarioExistente(nombreUsuario) {
-  return usuarios.some(user => user.usuario.toLowerCase() === nombreUsuario.toLowerCase());
+  console.log('la lista actual de usuarios en la "DB" es:', usuarios);
 }
--- main.js
+++ main.js
@@ -2,54 +2,30 @@
 Defino variables y constantes globales.
 */
 import { usuarios } from './DB/usuarios.js';
+import { 
+  mostrarUsuariosEnConsola,
+  inputUsuario,
+  inputEmail,
+  inputRol,
+  inputContraseña
+} from './funciones/functions.js';
 let continuar;
-const rolesValidos = ["administrador", "editor", "visualizador"];
 
 //Función para agregar un nuevo usuario a la "base de datos".
 function agregarNuevoUsuario() {
   const nuevoUsuario = {
       id: null,
       usuario: "",
+      email: "",
       rol: "",
-      contraseña: "",
-      email: ""
+      contraseña: ""
   };
 
   alert("Vamos a agregar un nuevo usuario. \nPor favor, ingrese los datos solicitados.");
-  nuevoUsuario.usuario = prompt(`Datos del usuario: \n\nUsuario: ${nuevoUsuario.usuario} \nEmail: ${nuevoUsuario.email} \nRol: ${nuevoUsuario.rol} \n\nIngrese el nombre de usuario:`);
-  //Valido que el usuario no exista en la "base de datos".
-  while (validarUsuarioExistente(nuevoUsuario.usuario)) {
-    alert(`El usuario "${nuevoUsuario.usuario}" ya existe. Por favor, ingrese un nombre de usuario diferente.`);
-    nuevoUsuario.usuario = prompt(`Datos del usuario: \n\nUsuario: ${nuevoUsuario.usuario} \nEmail: ${nuevoUsuario.email} \nRol: ${nuevoUsuario.rol} \n\nIngrese el nombre de usuario:`);
-  }
-  //HTML tiene la validación del campo email, aca vamos a validar que al menos tenga un "@" y un ".".
-  while (!nuevoUsuario.email.includes("@") || !nuevoUsuario.email.includes(".")) {
-    nuevoUsuario.email = prompt(`Datos del usuario: \n\nUsuario: ${nuevoUsuario.usuario} \nEmail: ${nuevoUsuario.email} \nRol: ${nuevoUsuario.rol} \n\nEl formato correcto es: "correo@electronico.com"\n\nIngrese el correo electrónico:`);
-    if (!nuevoUsuario.email.includes("@") || !nuevoUsuario.email.includes(".")) {
-      alert(`El Email ingresado: "${nuevoUsuario.email}" es inválido.\nIntente con un formato como éste ejemplo: "correo@electronico.com"`);
-      nuevoUsuario.email = "";
-    }
-  }
-  //Definimos en las variables globales que los roles válidos son "administrador", "editor" y "visualizador". validamos que el rol ingresado sea uno de esos tres y si llegaron a meter mayuscula lo pasamos a minuscula.
-  while (!rolesValidos.includes(nuevoUsuario.rol.toLowerCase())) {
-      nuevoUsuario.rol = prompt(`Datos del usuario: \n\nUsuario: ${nuevoUsuario.usuario} \nEmail: ${nuevoUsuario.email} \nRol: ${nuevoUsuario.rol} \n\nLos roles permitidos son:\n"${rolesValidos[0]}", "${rolesValidos[1]}" y "${rolesValidos[2]}"\n\n ¿Qué rol tendría el nuevo usuario?:`).toLowerCase();
-      if (!rolesValidos.includes(nuevoUsuario.rol)) {
-          alert(`El rol ingresado: "${nuevoUsuario.rol}" es inválido. \n\nLos roles permitidos son:\n"${rolesValidos[0]}", "${rolesValidos[1]}" y "${rolesValidos[2]}"\n\n Ingrese un rol válido.`);
-      }
-  }
-  let contraseña = "";
-  let confirmacionContraseña = "";
-  //Uso variables auxiliares de "contraseña" y "confirmacionContraseña" para pedir que se ingrese dos veces la contraseña y validar que coincidan. Y recien ahi lo ingreso en el objeto nuevoUsuario.
-  do {
-    contraseña = prompt(`Datos del usuario: \n\nUsuario: ${nuevoUsuario.usuario} \nEmail: ${nuevoUsuario.email} \nRol: ${nuevoUsuario.rol} \n\nAhora ingrese la contraseña:`);
-    confirmacionContraseña = prompt(`Datos del usuario: \n\nUsuario: ${nuevoUsuario.usuario} \nEmail: ${nuevoUsuario.email} \nRol: ${nuevoUsuario.rol} \n\nVuelva a ingresar la contraseña:`);
-
-    if (contraseña !== confirmacionContraseña) {
-      alert(`Las contraseñas ingresadas "${contraseña}" y "${confirmacionContraseña}" no coinciden. Intente nuevamente.`);
-    }
-  } while (contraseña !== confirmacionContraseña);
-  nuevoUsuario.contraseña = contraseña;
-  alert("Contraseña confirmada correctamente.");
+  nuevoUsuario.usuario = inputUsuario(nuevoUsuario);
+  nuevoUsuario.email = inputEmail(nuevoUsuario);
+  nuevoUsuario.rol = inputRol(nuevoUsuario);
+  nuevoUsuario.contraseña = inputContraseña(nuevoUsuario);
   //Asumo un número de ID autoincremental.
   nuevoUsuario.id = usuarios.length + 1;
   alert(`¿confirma los datos ingresados?: \n\nUsuario: ${nuevoUsuario.usuario}\nEmail: ${nuevoUsuario.email}\nRol: ${nuevoUsuario.rol}\nContraseña: ${"*".repeat(nuevoUsuario.contraseña.length)}\n\nPresione Aceptar para confirmar o Cancelar para abortar.`);
@@ -60,10 +36,9 @@ function agregarNuevoUsuario() {
 
 
 // Ejecución del main.
-alert("Bienvenido al sistema de ABM de Usuarios.");
+alert("Bienvenido al sistema de ABM de Usuarios. \n\n(Recuerde observar la consola)");
 continuar = confirm("¿Desea agregar un nuevo usuario?");
 if (continuar) {
-    startDB()
     mostrarUsuariosEnConsola()
     agregarNuevoUsuario();
 } else {
